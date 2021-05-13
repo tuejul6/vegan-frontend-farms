1b6b195e56aaf1a131c128b03e985f5988f48e81
veganswap-defi <vegandefi@gmail.com>
2021-05-13 14:55:16 +1000

    Fix redirects

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import HttpsRedirect from 'react-https-redirect'
 import App from './App'
 import Providers from './Providers'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <HttpsRedirect>
+      <Providers>
+        <App />
+      </Providers>
+    </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root'),
 )
